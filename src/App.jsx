bc2d9a8d13ebc82b3cb1e435bf5c69095450b3dc
sauryakhadka <khadkasaurya02@gmail.com>
2026-03-09 13:14:34 +0545

    Added home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
-import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Auth from "./pages/Auth";
+import Checkout from "./pages/Checkout";
+import "./App.css";
+import Navbar from "./components/NavBar";
 
 export default function App() {
-  return <div></div>;
+  return (
+    <div className="app">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/checkout" element={<Checkout />} />
+      </Routes>
+    </div>
+  );
 }
