b6c02cf1f0bdef32589651b5d66ddce0840c6655
sauryakhadka <khadkasaurya02@gmail.com>
2026-03-09 22:12:31 +0545

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,22 @@ import "./App.css";
 import Navbar from "./components/NavBar";
 import AuthProvider from "./context/AuthContext";
 import ProductDetails from "./pages/ProductDetails";
+import CartProvider from "./context/CartContext";
 
 export default function App() {
   return (
     <AuthProvider>
-      <div className="app">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/auth" element={<Auth />} />
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/products/:id" element={<ProductDetails />} />
-        </Routes>
-      </div>
+      <CartProvider>
+        <div className="app">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/auth" element={<Auth />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/products/:id" element={<ProductDetails />} />
+          </Routes>
+        </div>
+      </CartProvider>
     </AuthProvider>
   );
 }
